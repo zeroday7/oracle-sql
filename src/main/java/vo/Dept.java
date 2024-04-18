370ef411cb61735c0be52c6eff197ac764b63a38
GDJ <GDJ@GD>
2024-04-18 10:15:32 +0900

    dept,emp테이블 VO생성

---
+++ src/main/java/vo/Dept.java
@@ -0,0 +1,9 @@
+package vo; 
+
+// VO(Value Object) > DTO(Data Transfer Object) , Domain
+
+public class Dept {
+	public int deptNo;
+	public String dname;
+	public String loc;
+}
---
+++ src/main/java/vo/Emp.java
@@ -0,0 +1,12 @@
+package vo;
+
+public class Emp {
+	public int empNo;
+	public String ename;
+	public String job;
+	public int mgr;
+	public String hiredate;
+	public double sal;
+	public double comm;
+	public int deptno;
+}
